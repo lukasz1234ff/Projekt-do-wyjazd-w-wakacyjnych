ddf7a6a63bfea3cfd58858ddba07f5a4cfe2f564
lukasz1234ff <lukaszsidorowicz24@gmail.com>
2023-04-01 18:47:07 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "To jest mój pierwszy łańcuch znaków! Potocznie \"string\".";
+        System.out.println(message);
+    }
+}
+
+git
