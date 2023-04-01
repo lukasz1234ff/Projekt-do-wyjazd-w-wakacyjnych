0613991ef3477ee4be9969453ae12aa6b7f68413
lukasz1234ff <78597784+lukasz1234ff@users.noreply.github.com>
2023-04-01 18:48:58 +0200

    Update Main.java
    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        String message = "To jest mój pierwszy łańcuch znaków! Potocznie \"string\".";
+        String message = "To jest mój pierwszy łańcuch znakówhhhhhhhe \"string\".";
         System.out.println(message);
     }
 }
